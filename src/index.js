7333141fbd864703f3c30c10971889b0f2f5c94e
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-08-19 19:45:09 +0530

    added 4th paramenter for read more

--- src/index.js
+++ src/index.js
@@ -1,44 +1,50 @@
-//environment
 window.ProtoEmbed = window.ProtoEmbed || {};
-
-ProtoEmbed.initFrame = function (element, url, mode) {
-  if(typeof element == "string"){
-    element = document.getElementById(element);
+ProtoEmbed.initFrame = function(element, url, mode, readMoreEnabled) {
+  if (typeof element == "string") {
+      element = document.getElementById(element)
   }
-  element.removeChild(element.querySelector("blockquote"));
-
+  var blockquote = element.querySelector("blockquote");
+  if (blockquote) {
+      element.removeChild(blockquote);
+  }
   var ResizeService = Oasis.Service.extend({
     initialize: function() {
-      this.request('receive', mode).then(function (data) {
-        resizeIframe(element.querySelector("iframe"), data);
-      });
+        var that = this;
+        setTimeout(function(){
+            that.send("get_size");
+        }, 1000);
+        this.request('receive', mode, readMoreEnabled).then(function(data) {
+            resizeIframe(element.querySelector("iframe"), data)
+        })
     },
     events: {
-      resize_frame: function(data){
-        resizeIframe(element.querySelector("iframe"), data);
-      }
+        resize_frame: function(data) {
+            resizeIframe(element.querySelector("iframe"), data)
+        }
     }
   });
   var sandbox = oasis.createSandbox({
-    url: url,
-    type: 'html',
-    capabilities: [ 'receive' ],
-    services: {
-      receive: ResizeService
-    }
+      url: url,
+      type: 'html',
+      capabilities: ['receive'],
+      services: {
+          receive: ResizeService
+      }
   });
-  sandbox.el.setAttribute("sandbox", "allow-popups allow-scripts allow-same-origin")
-  element.append(sandbox.el);
+  sandbox.el.setAttribute("sandbox", "allow-popups allow-scripts allow-same-origin allow-presentation")
+  element.appendChild(sandbox.el);
   element.querySelector("iframe").style.width = '100%';
   element.querySelector("iframe").style.height = 'auto';
   element.querySelector("iframe").style.borderWidth = '0px'
 
   function resizeIframe(obj, data) {
-    // console.log(obj, data, "iframe object")
-    if (data !== undefined) {
-      // obj[0].style.height = obj.context.body.scrollHeight + 'px';
-      obj.style.height = data.height + 'px';
-      obj.style.width = '100%';
-    }
+      if (data !== undefined) {
+          obj.style.height = data.height + 'px';
+          obj.style.width = typeof (data.width) !== undefined ? data.width : '100%';
+      }
+  }
+
+  return {
+      sandbox: sandbox
   }
 }
